3134fcc8e831337e62a4d71155bbf1b01e6a1521
Honzasko <straka.jan5@seznam.cz>
2022-06-20 18:08:08 +0200

    v1.0

--- server.py
+++ server.py
@@ -9,7 +9,6 @@ app = Flask(__name__, static_folder="static")
 
 home = open("templates/home.html","r").read()
 home = home.replace("HOST",host)
-about_content = open("data/about.txt","r").read()
 
 print("Loading files complete")
 
@@ -20,6 +19,10 @@ def index():
 @app.route('/about/')
 def about_page():
   return render_template('about.html',title="O mně")
+
+@app.route("/social/")
+def social_page():
+    return render_template("social.html",title="Socialní sítě")
 
 app.run(debug=True,host="0.0.0.0",port=8080)
 
